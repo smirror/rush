dd294f5920a01fc7435752e2ba2fbfdfd8e26767
libra <23025996+smirror@users.noreply.github.com>
2023-08-24 21:43:52 +0900

    add syscall

---
+++ src/shell.rs
@@ -0,0 +1,33 @@
+use crate::helper::DynError;
+use nix::{
+    libc,
+    sys::{
+        signal::{killpg, signal, SigHandler, Signal},
+        wait::{waitpid, WaitPidFlag, WaitStatus},
+    },
+    unistd::{self, dup2, execvp, fork, pipe, setpgid, tcgetpgrp, tcsetpgrp, ForkResult, Pid},
+};
+use rustyline::{error::ReadlineError, Editor};
+use signal_hook::{consts::*, iterator::Signals};
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    ffi::CString,
+    mem::replace,
+    path::PathBuf,
+    process::exit,
+    sync::mpsc::{channel, sync_channel, Receiver, Sender, SyncSender},
+    thread,
+};
+
+/// システムコール呼び出しのラッパ。EINTRならリトライ
+fn syscall<F, T>(f: F) -> Result<T, nix::Error>
+    where
+        F: Fn() -> Result<T, nix::Error>,
+{
+    loop {
+        match f() {
+            Err(nix::Error::EINTR) => (), // リトライ
+            result => return result,
+        }
+    }
+}
