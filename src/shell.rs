bf5abd004a8597606589cf4f9d9ed97b64d8837e
libra <23025996+smirror@users.noreply.github.com>
2023-08-24 21:49:10 +0900

    add shell main

--- src/shell.rs
+++ src/shell.rs
@@ -31,6 +31,85 @@ enum ShellMsg {
     Quit(i32),     // シェルを終了。i32はシェルの終了コード
 }
 
+#[derive(Debug)]
+pub struct Shell {
+    logfile: String, // ログファイル
+}
+
+impl Shell {
+    pub fn new(logfile: &str) -> Self {
+        Shell {
+            logfile: logfile.to_string(),
+        }
+    }
+
+    /// mainスレッド
+    pub fn run(&self) -> Result<(), DynError> {
+        // SIGTTOUを無視に設定しないと、SIGTSTPが配送される
+        unsafe { signal(Signal::SIGTTOU, SigHandler::SigIgn).unwrap() };
+
+        let mut rl = Editor::<()>::new()?;
+        if let Err(e) = rl.load_history(&self.logfile) {
+            eprintln!("rush: ヒストリファイルの読み込みに失敗: {e}");
+        }
+
+        // チャネルを生成し、signal_handlerとworkerスレッドを生成
+        let (worker_tx, worker_rx) = channel();
+        let (shell_tx, shell_rx) = sync_channel(0);
+        spawn_sig_handler(worker_tx.clone())?;
+        Worker::new().spawn(worker_rx, shell_tx);
+
+        let exit_val; // 終了コード
+        let mut prev = 0; // 直前の終了コード
+        loop {
+            // 1行読み込んで、その行をworkerに送信
+            let face = if prev == 0 { '\u{1F642}' } else { '\u{1F480}' };
+            match rl.readline(&format!("rush {face} %> ")) {
+                Ok(line) => {
+                    let line_trimed = line.trim(); // 前後の空白文字を削除
+                    if line_trimed.is_empty() {
+                        continue; // 空のコマンドの場合は再読み込み
+                    } else {
+                        rl.add_history_entry(line_trimed); // ヒストリファイルに追加
+                    }
+
+                    worker_tx.send(WorkerMsg::Cmd(line)).unwrap(); // workerに送信
+                    match shell_rx.recv().unwrap() {
+                        ShellMsg::Continue(n) => prev = n, // 読み込み再開
+                        ShellMsg::Quit(n) => {
+                            // シェルを終了
+                            exit_val = n;
+                            break;
+                        }
+                    }
+                }
+                Err(ReadlineError::Interrupted) => eprintln!("rush: 終了はCtrl+D"),
+                Err(ReadlineError::Eof) => {
+                    worker_tx.send(WorkerMsg::Cmd("exit".to_string())).unwrap();
+                    match shell_rx.recv().unwrap() {
+                        ShellMsg::Quit(n) => {
+                            // シェルを終了
+                            exit_val = n;
+                            break;
+                        }
+                        _ => panic!("exitに失敗"),
+                    }
+                }
+                Err(e) => {
+                    eprintln!("rush: 読み込みエラー\n{e}");
+                    exit_val = 1;
+                    break;
+                }
+            }
+        }
+
+        if let Err(e) = rl.save_history(&self.logfile) {
+            eprintln!("rush: ヒストリファイルの書き込みに失敗: {e}");
+        }
+        exit(exit_val);
+    }
+}
+
 /// システムコール呼び出しのラッパ。EINTRならリトライ
 fn syscall<F, T>(f: F) -> Result<T, nix::Error>
     where
